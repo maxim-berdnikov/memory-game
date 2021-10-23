a31d39c900e400dc5e7ed59f01f3cc669d0f5e93
Maxim Berdnikov <m.radik25@gmail.com>
2021-10-23 21:29:44 +0300

    update logic, add imgs

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
 const section = document.querySelector(".js-container");
 const livesCount = document.querySelector(".js-lives");
 
-const defaultLives = 6
+const defaultLives = 6;
 
 let lives = defaultLives;
 livesCount.textContent = lives;
 
 const getData = () => [
-  { img: "linear-gradient(to bottom, red, purple)", name: "first" },
-  { img: "linear-gradient(to bottom, yellow, lime)", name: "second" },
-  { img: "linear-gradient(to bottom, green, teal)", name: "third" },
-  { img: "linear-gradient(to bottom, blue, purple)", name: "fourth" },
-  { img: "linear-gradient(to bottom, red, yellow)", name: "fifth" },
-  { img: "linear-gradient(to bottom, orange, lime)", name: "sixth" },
-  { img: "linear-gradient(to bottom, green, blue)", name: "seventh" },
-  { img: "linear-gradient(to bottom, teal, purple)", name: "eighth" },
-  { img: "linear-gradient(to bottom, red, purple)", name: "first" },
-  { img: "linear-gradient(to bottom, yellow, lime)", name: "second" },
-  { img: "linear-gradient(to bottom, green, teal)", name: "third" },
-  { img: "linear-gradient(to bottom, blue, purple)", name: "fourth" },
-  { img: "linear-gradient(to bottom, red, yellow)", name: "fifth" },
-  { img: "linear-gradient(to bottom, orange, lime)", name: "sixth" },
-  { img: "linear-gradient(to bottom, green, blue)", name: "seventh" },
-  { img: "linear-gradient(to bottom, teal, purple)", name: "eighth" },
+  { img: `url("./img/img1.jpeg")`, name: "first" },
+  { img: `url("./img/img2.jpeg")`, name: "second" },
+  { img: `url("./img/img3.jpg")`, name: "third" },
+  { img: `url("./img/img4.jpeg")`, name: "fourth" },
+  { img: `url("./img/img5.jpeg")`, name: "fifth" },
+  { img: `url("./img/img6.jpg")`, name: "sixth" },
+  { img: `url("./img/img7.jpeg")`, name: "seventh" },
+  { img: `url("./img/img8.jpeg")`, name: "eighth" },
+  { img: `url("./img/img1.jpeg")`, name: "first" },
+  { img: `url("./img/img2.jpeg")`, name: "second" },
+  { img: `url("./img/img3.jpg")`, name: "third" },
+  { img: `url("./img/img4.jpeg")`, name: "fourth" },
+  { img: `url("./img/img5.jpeg")`, name: "fifth" },
+  { img: `url("./img/img6.jpg")`, name: "sixth" },
+  { img: `url("./img/img7.jpeg")`, name: "seventh" },
+  { img: `url("./img/img8.jpeg")`, name: "eighth" },
 ];
+
+const firstShow = () => {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    setTimeout(() => {
+      card.classList.add("card-active");
+    }, 500);
+
+    setTimeout(() => {
+      card.classList.remove("card-active");
+    }, 10000);
+  });
+};
 
 const randomizeData = () => {
   const cardData = getData();
@@ -32,7 +46,6 @@ const randomizeData = () => {
 };
 
 const cardGenerator = () => {
-
   section.innerHTML = "";
 
   const cardData = randomizeData();
@@ -57,40 +70,61 @@ const cardGenerator = () => {
       checkCards(e);
     });
   });
+
+  firstShow();
+};
+
+const notification = (status) => {
+  confirm(status);
+};
+
+const restart = (status) => {
+  confirm(status);
+  const cards = document.querySelectorAll(".card");
+  cards.forEach((card) => {
+    if (card.classList.contains("card-active")) {
+      card.classList.remove("card-active");
+    }
+  });
+  
+  lives = defaultLives;
+  livesCount.textContent = lives;
+
+  setTimeout(() => {
+    cardGenerator();
+    firstShow();
+  }, 1000);
 };
 
 const checkCards = (e) => {
   const clickedCard = e.target;
-  clickedCard.classList.add('flipped');
-  const flippedCard = document.querySelectorAll('.flipped');
+  clickedCard.classList.add("flipped");
+  const flippedCard = document.querySelectorAll(".flipped");
   if (flippedCard.length === 2) {
     if (flippedCard[0].dataset.name === flippedCard[1].dataset.name) {
-        flippedCard.forEach((card) => {
-            card.classList.remove('flipped');
-        });
-    } else {
-        flippedCard.forEach((card) => {
-            card.classList.remove('flipped');
-            setTimeout(() => { card.classList.remove('card-active')}, 1000);
-        });
-        --lives;
-        livesCount.textContent = lives;
+      flippedCard.forEach((card) => {
+        card.classList.remove("flipped");
+      });
 
-        if (lives === 0) {
-            const cards = document.querySelectorAll('.card');
-            cards.forEach((card) => {
-                if (card.classList.contains('card-active')) {
-                    card.classList.remove('card-active')
-                }
-            });
-            lives = defaultLives;
-            livesCount.textContent = lives;
-           
-            setTimeout(() => { 
-                cardGenerator();
-            }, 1000);
-           
-        }
+      const activeCards = document.querySelectorAll(".card-active");
+      if (activeCards.length === 16) {
+        setTimeout(() => restart("Yay!"), 800);
+        // restart();
+      }
+    } else {
+      flippedCard.forEach((card) => {
+        card.classList.remove("flipped");
+        setTimeout(() => {
+          card.classList.remove("card-active");
+        }, 1000);
+      });
+      --lives;
+      livesCount.textContent = lives;
+
+      if (lives === 0) {
+        // notification("You are failed(");
+        restart("You are failed(");
+      }
     }
   }
 };
